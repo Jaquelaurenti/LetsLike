d590b69d1ab49b03877131778f8d75d9e51e5c95
Jaque Laurenti <jaqueline.laurenti@midway.com.br>
2021-11-18 18:18:23 -0300

    First Commit

---
+++ LetsLike/Data/LetsLikeContext.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsLike.Data
+{
+    public class LetsLikeContext : DbContext
+    {
+        public LetsLikeContext(DbContextOptions<LetsLikeContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                   var connection = @"Server=ARKMF98769\SQLEXPRESS;Database=myDataBase;Trusted_Connection=True;";
+
+                    optionsBuilder.UseSqlServer(connection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        
+        }
+
+
+    }
+}
---
+++ LetsLike/Startup.cs
@@ -0,0 +1,74 @@
+using LetsLike.Configurations;
+using LetsLike.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsLike
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<LetsLikeContext>(
+              options => options.
+              UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LetsLike", Version = "v1" });
+            });
+
+            // Adicionando as inversões de controle
+            RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LetsLike v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            Factory.RegisterServices(services);
+        }
+    }
+}
